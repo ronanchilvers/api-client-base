901188f45994931cbf41ca4d6083a42f447086d7
Ronan Chilvers <ronan@d3r.com>
2016-01-20 15:26:05 +0000

    Add concrete credentials class

---
+++ src/Credentials.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Ronanchilvers\ApiClientBase;
+
+/**
+ * Standard credentials object
+ * 
+ * @author Ronan Chilvers <ronan@d3r.com>
+ */
+class Credentials implements CredentialsInterface
+{
+    /**
+     * The username
+     * 
+     * @var string
+     */
+    private $username;
+
+    /**
+     * The password
+     * 
+     * @var string
+     */
+    private $password;
+
+    /**
+     * Class constructor
+     *
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    /**
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
